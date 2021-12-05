b74f7492b7a66d966aa860095c8bc390383c4f79
sergey <serezha.shabalin.95@mail.ru>
2021-12-05 15:31:28 +0300

    jr

--- src/components/department.jsx
+++ src/components/department.jsx
@@ -4,35 +4,62 @@ import React from 'react'
 const Departments = () => {
     const [numbers, setNumbers] = React.useState(
         [{
+            id: 1,
             name: 'отдел информационного обеспечения',
-            level: 4,
-            boss: 'Ковалёв Василий Николаевич',
-            cabinetNumber: 412
+            level: '4',
+            bossName: 'Василий',
+            bossSurName: 'Ковалёв',
+            bossMiddleName: 'Вячеславович',
+            cabinetNumber: '412'
         },
             {
+                id: 2,
                 name: 'отдел хозяйственного обеспечеия',
-                level: 4,
-                boss: 'Кузнецов Андрей Иванович',
-                cabinetNumber: 412
+                level: '3',
+                bossName: 'Кузнецов',
+                bossSurName: 'Андрей',
+                bossMiddleName: 'Иванович',
+                cabinetNumber: '212'
             }
         ]
     )
 
-    const [count, setCount] = React.useState(0)
+    const addDepartment = ()=>{
+
+    }
+
+    let res = numbers.map(function(item) {
+        return (
+            <tr key={item.id}>
+                    <th scope="row">{item.id}</th>
+                    <td>{item.name}</td>
+                    <td>{item.level}</td>
+                    <td>{item.bossName} {item.bossSurName} {item.bossMiddleName}</td>
+                    <td>{item.cabinetNumber}</td>
+                </tr>)
+    })
+
+    return <div>
+        <p>
+            Отделы
+            <table className="table-bordered ">
+                <thead className="table-secondary">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">name</th>
+                    <th scope="col">level</th>
+                    <th scope="col">boss</th>
+                    <th scope="col">cabinet</th>
+                </tr>
+                </thead>
+                {res}
+                <button className="btn btn-success" onClick={addDepartment}>Добавить новый отдел</button>
+            </table>
+        </p>
+
+    </div>
 
 
-    return (
-        <div>
-            {numbers}
-            <ul>
-                {
-                    numbers.map((name,cabinetNumber,level) => <li key={cabinetNumber}>{numbers}</li>)
-                }
-            </ul>
-             {/*<button onClick={addNumbers}>новое число</button>*/}
-             {/*<button onClick={addCount}>прибавить</button>*/}
-        </div>
-    )
 }
 
 export default Departments
--- src/pages/department.js
+++ src/pages/department.js
@@ -4,14 +4,15 @@ import Departments from "../components/department";
 
 export const Department = () => {
     return (
-        <div className="jumbotron">
-            <div className="container">
-                <h1 className="display-4">Отделы</h1>
-               <h2>
-                   {<Departments/>}
-               </h2>
+        <div>
+            <div className="jumbotron">
+                <div className="container">
+                </div>
 
             </div>
+            <h2>
+                {<Departments/>}
+            </h2>
         </div>
     )
 }
