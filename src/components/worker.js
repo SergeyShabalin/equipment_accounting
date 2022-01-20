ea93abf61101665949671c6658409ad19d4dbeb3
SergeyShabalin <obaldevshiy@yandex.ru>
2022-01-20 11:30:45 +0300

    info about every worker

--- src/components/worker.js
+++ src/components/worker.js
@@ -4,20 +4,30 @@ import $api from "../API/index";
 
 export const Worker = () => {
 
-const location = useLocation()
+    const [user, setUser] = React.useState({})
+    const location = useLocation()
 
 
-    console.log(location.pathname.split('/')[2])
-async function getUserData(){
-    $api.get('http://localhost:1000/api')
-}
+    async function getUser() {
+        let userId = location.pathname.split('/')[2]
+        let resp = await $api.get('/users/' + userId)
+        setUser(resp.data)
+    }
+
+    React.useEffect(() => {
+        getUser()
+    }, [])
+
+
+
+
     return (
         <div>
-            {/*<h2>Фамилия: {initState.bossSurName}</h2>*/}
-            {/*<h2>Имя: {initState.bossName}</h2>*/}
-            {/*<h2>Отчество:{initState.bossMiddleName}</h2>*/}
-            {/*<h2>email: {initState.email}</h2>*/}
-        {/*<button onClick={transferInfoWork}>тык</button>*/}
+
+            <h2>Фамилия: {user.surname}</h2>
+            <h2>Имя: {user.name}</h2>
+            <h2>Отчество: {user.lastname}</h2>
+            <h2>email: {user.email}</h2>
         </div>
     )
 }
